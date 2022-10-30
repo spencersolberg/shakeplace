1b473f3607d28600ac2018c4bd73e2b132d71ed9
Spencer <other.spencersolberg@outlook.com>
2022-10-30 15:47:13 -0400

    Unnecessary class removed

--- app/lib/components/pixelTable.tsx
+++ app/lib/components/pixelTable.tsx
@@ -7,7 +7,7 @@ import { constructMessage, getAltColor } from "~/lib/utils";
 export default function PixelTable({ pixels }: { pixels: Pixel[] }) {
 	return (
 		<>
-			<div className="mx-auto grid grid-cols-7  border-black max-w-sm mb-1">
+			<div className="mx-auto grid grid-cols-7 max-w-sm mb-1">
 				<h2 className="font-semibold text-center">X</h2>
 				<h2 className="font-semibold text-center">Y</h2>
 				<h2 className="font-semibold text-center">Color</h2>
@@ -27,24 +27,40 @@ interface RowProps {
 function Row({ data, index, style }: RowProps) {
 	const pixel = data[index];
 	return (
-		<div className="min-w-full grid grid-cols-7 border-t border-r border-l border-black px-2" style={{
-			...style,
-			backgroundColor: "#" + pixel.color,
-			color: getAltColor("#" + pixel.color)
-		}}>
+		<div
+			className="min-w-full grid grid-cols-7 border-t border-r border-black border-l px-2"
+			style={{
+				...style,
+				backgroundColor: "#" + pixel.color,
+				color: getAltColor("#" + pixel.color)
+			}}
+		>
 			<h2 className="text-center">{pixel.x}</h2>
 			<h2 className="text-center">{pixel.y}</h2>
-			<h2 className="text-center">#{pixel.color}</h2>
-			<h2 className="text-center text-xs my-auto col-span-2">{new Date(pixel.placedAt).toLocaleDateString()}</h2>
-			<Link to={`/${pixel.x},${pixel.y}`} prefetch="intent" className="text-center underline hover:italic">{pixel.active ? "✔" : "✖"}</Link>
-			<Link to={`/pixel/${pixel.id}`} prefetch="intent" className="text-center underline hover:italic">Verify</Link>
+			<h2 className="text-center">{pixel.color}</h2>
+			<h2 className="text-center text-xs my-auto col-span-2">
+				{new Date(pixel.placedAt).toLocaleDateString()}
+			</h2>
+			<Link
+				to={`/${pixel.x},${pixel.y}`}
+				prefetch="intent"
+				className="text-center underline hover:italic"
+			>
+				{pixel.active ? "✔" : "✖"}
+			</Link>
+			<Link
+				to={`/pixel/${pixel.id}`}
+				prefetch="intent"
+				className="text-center underline hover:italic"
+			>
+				Verify
+			</Link>
 		</div>
-
 	);
 }
 
 interface PixelListProps {
-	data: Pixel[]
+	data: Pixel[];
 }
 function PixelList({ data }: PixelListProps) {
 	return (
@@ -60,4 +76,3 @@ function PixelList({ data }: PixelListProps) {
 		</List>
 	);
 }
-
